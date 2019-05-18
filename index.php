56b6356fe36299b5a4e34527ec193c42d6401e3d
eryzha <33304669+eryzha@users.noreply.github.com>
2019-05-18 23:23:31 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     
     // PHP Data Objects(PDO) Sample Code:
 	try {
-    	$conn = new PDO("mssql:server = tcp:ermawebsub1.database.windows.net,1433; Database = ermawebsub1", "erma", "Zha3254sub1");
+    	$conn = new PDO("sqlsrv:server = ermawebsub1.database.windows.net,1433; Database = ermawebsub1", "erma", "Zha3254sub1");
     	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch (PDOException $e) {
@@ -39,8 +39,8 @@
 
 	// SQL Server Extension Sample Code:
 	$connectionInfo = array("UID" => "erma@ermawebsub1", "pwd" => "Zha3254sub1", "Database" => "ermawebsub1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-	$serverName = "tcp:ermawebsub1.database.windows.net,1433";
-	$conn = mssql_connect($serverName, $connectionInfo);
+	$serverName = "ermawebsub1.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
 
     if (isset($_POST['submit'])) {
         try {
