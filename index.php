39516d5c802bcb0e39ed2dafffaee6f7c93d172e
eryzha <33304669+eryzha@users.noreply.github.com>
2019-05-18 22:29:47 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -26,21 +26,17 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    
-    // PHP Data Objects(PDO) Sample Code:
-	try {
-    	$conn = new PDO("sqlsrv:server = tcp:ermawebsub1.database.windows.net,1433; Database = ermawebsub1", "erma", "Zha3254sub1");
-    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-	catch (PDOException $e) {
-	    print("Error connecting to SQL Server.");
-	    die(print_r($e));
-	}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "erma@ermawebsub1", "pwd" => "Zha3254sub1", "Database" => "ermawebsub1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:ermawebsub1.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+ 
+ 	$host = "ermawebsub1.database.windows.net";
+    $user = "erma";
+    $pass = "Zha3254sub1";
+    $db = "ermawebsub1";
+    try {
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
 
     if (isset($_POST['submit'])) {
         try {
